5c65e3440d1a43259b0ddfb9352026b9078495ee
Pawsative <8021703+Pawsative@users.noreply.github.com>
2021-02-17 01:14:57 -0500

    re-added arbitraryTableValue
    The version of mysqlite on DarkRP doesn't seem to use arbitraryTableValue?

--- mysqlite.lua
+++ mysqlite.lua
@@ -154,6 +154,11 @@ loadMySQLModule()
 
 module("MySQLite")
 
+-- Helper function to return the first value found when iterating over a table.
+-- Replaces the now deprecated table.GetFirstValue
+local function arbitraryTableValue(tbl)
+    for _, v in pairs(tbl) do return v end
+end
 
 function initialize(config)
     MySQLite_config = config or MySQLite_config
@@ -275,7 +280,7 @@ local function msOOQuery(sqlText, callback, errorCallback, queryValue)
     end
 
     queryObject.onSuccess = function()
-        local res = queryValue and data and data[1] and table.GetFirstValue(data[1]) or not queryValue and data or nil
+        local res = queryValue and data and data[1] and arbitraryTableValue(data[1]) or not queryValue and data or nil
         if callback then callback(res, queryObject:lastInsert()) end
     end
     queryObject:start()
@@ -291,7 +296,7 @@ local function tmsqlQuery(sqlText, callback, errorCallback, queryValue)
         end
 
         if not res.data or #res.data == 0 then res.data = nil end -- compatibility with other backends
-        if queryValue and callback then return callback(res.data and res.data[1] and table.GetFirstValue(res.data[1]) or nil) end
+        if queryValue and callback then return callback(res.data and res.data[1] and arbitraryTableValue(res.data[1]) or nil) end
         if callback then callback(res.data, res.lastid) end
     end
 
